e1df601ec24baf3c56ccb2a044647ae9f70da5a7
Marcos Bustos Castillo <marcosgbc94@gmail.com>
2025-09-22 22:08:21 -0300

    Se agrega sistema de observabilidad

--- app/main.py
+++ app/main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from core.exceptions import ExceptionHandler
 from core.common import init_startup, get_app_name
 from presentation.api.router.v1 import user_router, auth_router 
+from prometheus_fastapi_instrumentator import Instrumentator
+from core.logging_config import setup_logging
+
+setup_logging()
 
 # Nombre de la API
 app = FastAPI(title=get_app_name())
+
+Instrumentator().instrument(app).expose(app)
 
 # Se incluyen los routers
 app.include_router(user_router.router)
